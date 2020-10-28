2a60b8e9c9e9d1c6bef9ebde1298f2d582c74ad2
Daniel Raffnsøe Dall <RaikoPrivate@hotmail.com>
2020-10-28 11:55:25 +0100

    refac

--- Lease.Domain/Shared/DomainExceptions.cs
+++ Lease.Domain/Shared/DomainExceptions.cs
@@ -8,17 +8,17 @@ namespace Lease.Domain.Shared
     {
         public class InvalidEntityState : Exception
         {
-            public InvalidEntityState(object entity, string message)
-                : base($"Entity {entity.GetType().Name} state change rejected, {message}")
+            public InvalidEntityState(object entity, string message) : base($"Entity {entity.GetType().Name} state change rejected, {message}")
             {
+
             }
         }
 
         public class ProfanityFound : Exception
         {
-            public ProfanityFound(string text)
-                : base($"Profanity found in text: {text}")
+            public ProfanityFound(string text) : base($"Profanity found in text: {text}")
             {
+
             }
         }
     }
