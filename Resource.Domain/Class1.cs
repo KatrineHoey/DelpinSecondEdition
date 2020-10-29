92c2c94790c203c5615685430f10e9887a4d3def
Allan Thanabalasingam <alla2265@edu.eal.dk>
2020-10-29 10:27:33 +0100

    da

--- Resource.Domain/Class1.cs
+++ Resource.Domain/Class1.cs
@@ -4,5 +4,6 @@ namespace Resource.Domain
 {
     public class Class1
     {
+        //slet denne klasse, også skal der tilføjes alle value objekter fra domæne modellen her. De skal oprettes i en normal klasse. 
     }
 }
