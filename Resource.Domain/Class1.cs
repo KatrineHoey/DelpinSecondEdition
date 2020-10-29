3539bcec4f0a6b12ef38c1f5cd1c8dbb34a329e2
Allan Thanabalasingam <alla2265@edu.eal.dk>
2020-10-29 10:30:39 +0100

    Revert "da"
    This reverts commit 92c2c94790c203c5615685430f10e9887a4d3def.

--- Resource.Domain/Class1.cs
+++ Resource.Domain/Class1.cs
@@ -4,6 +4,5 @@ namespace Resource.Domain
 {
     public class Class1
     {
-        //slet denne klasse, også skal der tilføjes alle value objekter fra domæne modellen her. De skal oprettes i en normal klasse. 
     }
 }
