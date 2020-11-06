9b1c73cc508ca42d04f4a4c5e57a19919043c85e
Allan Thanabalasingam <alla2265@edu.eal.dk>
2020-11-06 14:54:35 +0100

    Added yml file to resource

--- Resource.Microservice/Startup.cs
+++ Resource.Microservice/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace Resource.Microservice
 {
@@ -23,12 +24,12 @@ namespace Resource.Microservice
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
