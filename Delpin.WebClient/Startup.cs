8294612d13ab1f4fb158febad12584528e459c25
Katrine Andersen <Katrine.77@hotmail.com>
2020-11-07 11:15:50 +0100

    Searchicon

--- Delpin.WebClient/Startup.cs
+++ Delpin.WebClient/Startup.cs
@@ -30,12 +30,13 @@ namespace Delpin.WebClient
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-            services.AddSingleton<HttpClient>();
+            services.AddSingleton<HttpClient>();      
 
 
-        }
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
+    }
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
