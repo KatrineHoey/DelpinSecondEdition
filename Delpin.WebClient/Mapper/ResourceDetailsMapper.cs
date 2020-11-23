e88ee0997f19dedcab298fb92f2a45e7d73271a4
Daniel Raffnsøe Dall <RaikoPrivate@hotmail.com>
2020-11-23 10:34:10 +0100

    ResourceMapper and ViewModel

---
+++ Delpin.WebClient/Mapper/ResourceDetailsMapper.cs
@@ -0,0 +1,47 @@
+﻿using Delpin.WebClient.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delpin.WebClient.Mapper
+{
+    public class ResourceDetailsMapper
+    {
+        //public static ResourceViewModel.ResourceDetails Map(ResourceDto.ResourceDetails dto)
+        //{
+        //    if (dto == null)
+        //    { return null; }
+        //    return new ResourceViewModel.ResourceDetails
+        //    {
+        //        ResourceName = dto.ResourceName,
+        //        ResourceNo = dto.ResourceNo,
+        //        ResourcePrice = dto.ResourcePrice,
+        //        IsDeleted = dto.IsDeleted,
+        //    };
+        //}
+
+        //public static IEnumerable<ResourceViewModel.ResourceDetails> Map(IEnumerable<ResourceDto.ResourceDetails> model)
+        //{
+        //    return model.Select(x => Map(x)).AsEnumerable();
+        //}
+
+        //public static IEnumerable<ResourceDto.ResourceDetails> Map(IEnumerable<ResourceViewModel.ResourceDetails> model)
+        //{
+        //    return model.Select(x => Map(x)).AsEnumerable();
+        //}
+
+        //public static ResourceDto.ResourceDetails Map(ResourceViewModel.ResourceDetails model)
+        //{
+        //    if (model == null)
+        //    { return null; }
+        //    return new ResourceDto.ResourceDetails
+        //    {
+        //        ResourceName = dto.ResourceName,
+        //        ResourceNo = dto.ResourceNo,
+        //        ResourcePrice = dto.ResourcePrice,
+        //        IsDeleted = dto.IsDeleted,
+        //    };
+        //}
+    }
+}
---
+++ Delpin.WebClient/ViewModel/ResourceViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delpin.WebClient.ViewModel
+{
+    public class ResourceViewModel
+    {
+        public class ResourceDetails 
+        {
+            [Required(ErrorMessage = "Resource navn mangler.")]
+            public string ResourceName { get; set; }
+
+            [Required(ErrorMessage = "Resource nummer mangler.")]
+            public int ResourceNo { get; set; }
+
+            [Required(ErrorMessage = "Resource pris mangler.")]
+            public decimal ResourcePrice { get; set; }
+
+            [Required(ErrorMessage = "Er slettet mangler.")]
+            public bool IsDeleted { get; set; }
+        }
+    }
+}
