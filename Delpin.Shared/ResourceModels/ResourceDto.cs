f0f5dd87e136358035d6119a3390f087cf6fc605
Daniel Raffnsøe Dall <RaikoPrivate@hotmail.com>
2020-11-24 09:06:14 +0100

    ResourceDto

---
+++ Delpin.Shared/ResourceModels/ResourceDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delpin.Shared.ResourceModels
+{
+    public static class ResourceDto
+    {
+        public class ResourceDetails
+        {            
+            public string ResourceName { get; set; }
+            
+            public int ResourceNo { get; set; }
+            
+            public decimal ResourcePrice { get; set; }
+
+            public bool IsDeleted { get; set; }
+        }
+    }
+}
